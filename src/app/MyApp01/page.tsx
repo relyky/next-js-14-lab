ba4b50b98aa8cc833124d8741113fba5b090a2ae
Rely Ky <relyky@hotmail.com>
2024-03-21 11:43:38 +0800

    simple app routing

---
+++ src/app/MyApp01/page.tsx
@@ -0,0 +1,7 @@
+export default function MyApp01() {
+	return (
+		<main>
+			<h1>MyApp01 哈囉世界</h1>
+		</main>
+	)
+}
---
+++ src/app/MyApp02/page.tsx
@@ -0,0 +1,8 @@
+export default function MyApp02() {
+	return (
+		<main>
+			<h1>MyApp02</h1>
+			<h1>我出運了</h1>
+		</main>
+	)
+}
