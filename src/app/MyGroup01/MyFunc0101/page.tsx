98acda182c08ba024053bd2827386029a2f11297
Rely Ky <relyky@hotmail.com>
2024-03-21 21:11:43 +0800

    private files

--- src/app/MyGroup01/MyFunc0101/page.tsx
+++ src/app/MyGroup01/MyFunc0101/page.tsx
@@ -1,8 +1,13 @@
+import ATextField from "@/app/_widgets/ATextField";
+
 export default function MyFunc0101() {
 	return (
 		<article role='container'>
 			<h1>MyFunc0101 哈囉世界</h1>
 			<h2>這部份是SSR。</h2>
+
+			<ATextField />
+
 		</article>
 	)
 }
---
+++ src/app/_widgets/ATextField.tsx
@@ -0,0 +1,8 @@
+export default function ATextField()
+{
+	return (
+		<div>
+			A text field here in future.
+		</div>
+	)
+}
