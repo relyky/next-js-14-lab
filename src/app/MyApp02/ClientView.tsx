e3e86a4a5bdae0b2c2d1400b89d2192f69380c58
Rely Ky <relyky@hotmail.com>
2024-03-27 09:24:04 +0800

    SimpleCounter

--- src/app/MyApp02/ClientView.tsx
+++ src/app/MyApp02/ClientView.tsx
@@ -17,10 +17,11 @@ export default function ClientView() {
 	if (!isMounted) return; // 不想前端初始畫面被看到。
 	return (
 		<div className={ss.box2}>
+			<code>ClientView.tsx</code>
 			<h2>這部份是 CSR。</h2>
 			<p>CSR 才能使用 client 端資源。</p>
 			<SimpleCounter onChange={setCount} />
-			<h2>inner count: {count}</h2>
+			<p>the count in the SimpleCounter: {count}</p>
 		</div>
 	)
 }
--- src/app/MyApp02/page.tsx
+++ src/app/MyApp02/page.tsx
@@ -10,6 +10,7 @@ export default function MyApp02() {
 
 	return (
 		<article role="container" className={ss.box1}>
+			<code>page.tsx</code>
 			<h1>{metadata.title as string}</h1>
 			<h2>這部份是 SSR。<small>(外面也是 SSR)</small></h2>
 			<p>SSR 無法使用 client 端資源。</p>
