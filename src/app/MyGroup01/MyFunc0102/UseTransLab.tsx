44a52f45601eee62b0f5ada85b87ff1b41328562
Rely Ky <relyky@hotmail.com>
2024-03-27 18:03:28 +0800

    loading

--- src/app/MyGroup01/MyFunc0102/UseTransLab.tsx
+++ src/app/MyGroup01/MyFunc0102/UseTransLab.tsx
@@ -1,4 +1,5 @@
 "use client"
+import { useEffect, useState } from "react";
 import FilterList from "./FilterList";
 import FilterListDefer from "./FilterListDefer";
 import FilterListTrans from "./FilterListTrans";
@@ -6,9 +7,19 @@ import ss from './page.module.css'
 
 //-----------------------------------------------------------------------------
 // const names:string[] = ['a00b00','a01b01']
-const names: string[] = Array(10000).fill(1).map((_,i) => `a${i}b${i}`)
+// const names: string[] = Array(10000).fill(1).map((_,i) => `a${i}b${i}`)
 
 export default function UseTransLab() {
+	const [names, setNames] = useState<string[]>([])
+	
+	useEffect(() => {
+		const names: string[] = Array(10000).fill(1).map((_, i) => `a${i}b${i}`)
+		setNames(names)
+	}, [])
+
+	if (names.length === 0)
+		return (<h1 style={{ background: 'red' }}>init...</h1>)
+
 	return (
 		<div>
 			<h1>Heavy UI updates as urgent</h1>
