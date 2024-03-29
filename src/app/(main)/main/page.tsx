c65d8880555f4357411c38063590bd3ea08caa87
Rely Ky <relyky@hotmail.com>
2024-03-29 14:52:18 +0800

    main route

--- src/app/(main)/main/page.tsx
+++ src/app/(main)/main/page.tsx
@@ -1,9 +1,10 @@
 import Link from "next/link";
 
-export default function AboutPage() {
+export default function MainPage() {
 	return (
 		<article role="container">
-			<h1>關於我們 {new Date().toLocaleTimeString()}</h1>
+			<h1>主頁</h1>
+			<h2>登入後顯示。可在此頁顯示 KPI 資訊。</h2>
 			<Link href='/'>回到首頁</Link>
 		</article>
 	)
