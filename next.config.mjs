64e299ff008763d8eea1107fe3745ed5e73912ef
Rely Ky <relyky@hotmail.com>
2024-04-01 16:40:38 +0800

    deploy on IIS

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+
+	/// This file is used to configure Next.js, and get used by the Next.js server and build phases, but it’s not included in the browser build.
+	/// It can be used to configure rewrites, redirects, custom headers, environment variables etc.
+	// experimental: {
+	// 	serverActions: true,
+	// },
+	// reactStrictMode: true,
+	// swcMinify: true,
+};
 
 export default nextConfig;
