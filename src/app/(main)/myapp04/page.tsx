55285518adb5b9ca03eb01ceaf29a2c9cd4a8bec
Rely Ky <relyky@hotmail.com>
2024-04-08 11:23:18 +0800

    RecoilDevTools

--- src/app/(main)/myapp04/page.tsx
+++ src/app/(main)/myapp04/page.tsx
@@ -3,12 +3,12 @@ import AppForm from "./AppForm";
 import { RecoilRoot } from "recoil";
 import { RecoilDevTools } from "recoil-toolkit";
 
-/* 此時在 Server side */
+/* Recoil 只能在前端作用 */
 export default function MyApp04() {
 	return (
 		<RecoilRoot>
 			<AppForm />
-      <RecoilDevTools /* ※放在最後面才看得到初始狀態。 在 Release mode 也有效？ */ />
+			{process.env.NODE_ENV === 'development' && <RecoilDevTools /* ※放在最後面才看得到初始狀態 */ />}
 		</RecoilRoot>
 	)
 }
