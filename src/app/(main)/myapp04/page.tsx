fd95283118f1d743eda9bcfcdbf966ff2408cf89
Rely Ky <relyky@hotmail.com>
2024-04-12 17:21:26 +0800

    jotai - commit

--- src/app/(main)/myapp04/page.tsx
+++ src/app/(main)/myapp04/page.tsx
@@ -5,11 +5,11 @@ import AppForm from "./AppForm";
 
 const store = createStore();
 
-/* Recoil 只能在前端作用 */
+/* Jotai 只能在前端作用 */
 export default function MyApp04() {
 	return (
 		<Provider store={store}>
-			{process.env.NODE_ENV === 'development' && <DevTools store={store} position='bottom-right' />}
+			{process.env.NODE_ENV === 'development' && <DevTools store={store} />}
 			<AppForm />
 		</Provider>
 	)
